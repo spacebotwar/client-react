8ad047a0a0fc1e92a03520b0dbdada24326e0727
Iain C Docherty <iain@iain-docherty.com>
2015-12-28 04:14:31 -0500

    Start to implement WebSocket calls to User interface

--- src/app/js/stores/UserStore.jsx
+++ src/app/js/stores/UserStore.jsx
@@ -10,46 +10,10 @@ var UserStore = Reflux.createStore({
     init: function() {
         this.state = this.getInitialState();
 
+        // TODO this should come from a configuration
         this.ws = new WebSocket('ws://spacebotwar.com:5000/ws/user/');
         this.ws.onmessage = this.wsOnMessage;
         this.ws.onopen = this.wsOpen;
-    },
-
-    wsOnMessage: function (event) {
-        console.log("UserStore: message ["+event.data+"]");
-        var json = JSON.parse(event.data);
-        var func = json.route;
-        func = 'ws' + func.replace(/\//g,'_');
-        console.log("Userstore: func ["+func+"]");
-        this[func](json.content);
-    },
-
-    ws_: function(data) {
-        console.log("UserStore: ws_");
-    },
-    ws_client_code: function(data) {
-        console.log("UserStore: ws_client_code");
-        this.clientCode = data.client_code;
-        var userStore = JSON.stringify({
-            clientCode: this.clientCode
-        });
-        console.log("UserStore: set userStore ["+userStore+"]");
-        localStorage.setItem('UserStore', userStore) 
-    },
-
-    // TODO Look at doing this with promises.
-    wsOpen: function(event) {
-        this.state.wsState = 'NOT_CONNECTED';
-        // Validate the clientCode
-        var clientCode = this.state.clientCode || "bad";
-        console.log("UserStore: clientCode needs to be initialized");
-        this.ws.send(JSON.stringify({
-            "route":  "/client_code",
-            "content":  {
-                "msg_id" :      "123",
-                "client_code" : clientCode
-            }
-        }));
     },
 
     getInitialState: function() {
@@ -64,16 +28,64 @@ var UserStore = Reflux.createStore({
             wsState:        'NOT_CONNECTED'
         };
     },
+    // WebSocket, generic received message router
+    wsOnMessage: function (event) {
+        // TODO Better error checking for valid function names
+        // TODO Check for message error codes
+        console.log("UserStore: message ["+event.data+"]");
+        var json = JSON.parse(event.data);
+        var func = json.route;
+        func = 'ws' + func.replace(/\//g,'_');
+        console.log("Userstore: func ["+func+"]");
+        this[func](json.content);
+    },
+    // WebSocket welcome message
+    ws_: function(data) {
+        console.log("UserStore: ws_");
+    },
+    // WebSocket client_code message
+    ws_client_code: function(data) {
+        console.log("UserStore: ws_client_code");
+        this.clientCode = data.client_code;
+        var userStore = JSON.stringify({
+            clientCode: this.clientCode
+        });
+        console.log("UserStore: set userStore ["+userStore+"]");
+        localStorage.setItem('UserStore', userStore) 
+    },
+
+    // WebSocket connection made
+    wsOpen: function(event) {
+        // TODO Look at doing this with promises.
+        this.state.wsState = 'CONNECTED';
+
+        // Validate the clientCode
+        var clientCode = this.state.clientCode || "bad";
+        console.log("UserStore: clientCode needs to be initialized");
+        this.ws.send(JSON.stringify({
+            "route":  "/client_code",
+            "content":  {
+                "msg_id" :      "123",
+                "client_code" : clientCode
+            }
+        }));
+    },
+
     onLoginWithPassword: function(username, password) {
         console.log("UserStore: loginWithPassword ["+username+"]["+password+"]");
         _.assign(this.state, {
             username:   username,
             password:   password
         });
-        if (username == 'icy' && password == 'secret') {
-            console.log("UserStore: correct password");
-            this.state.mode = 'LOGGED_IN';
-        }
+        this.ws.send(JSON.stringify({
+            "route":        "/login_with_password",
+            "content": {
+                "client_code" :     this.state.clientCode,
+                "username" :        username,
+                "password" :        password
+            }
+        }));
+
         this.trigger(this.state);
         console.log("UserStore: after trigger");
     },
