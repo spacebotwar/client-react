5416bd45c42c955f87c65dbdc7ab335901beb76d
Iain C Docherty <iain@iain-docherty.com>
2015-12-28 19:08:27 -0500

    Moved async calls to API into the Action module

--- src/app/js/actions/UserActions.jsx
+++ src/app/js/actions/UserActions.jsx
@@ -7,7 +7,71 @@ var UserActions = Reflux.createActions([
     'loginWithEmail',
     'register',
     'logout',
+    'ws_client_code',
+    'ws_login_with_password',
+    'ws_register',
+    'ws_'
 ]);
+
+// TODO this should come from a configuration
+var ws = new WebSocket('ws://spacebotwar.com:5000/ws/user/');
+var clientCode = 'bad';
+
+ws.onmessage = function() {
+    // TODO Better error checking for valid function names
+    // TODO Check for message error codes
+    console.log("UserActions: message ["+event.data+"]");
+    var json = JSON.parse(event.data);
+    var func = json.route;
+    func = 'ws' + func.replace(/\//g,'_');
+    console.log("Userstore: func ["+func+"]");
+    // Convert the route into an action
+    UserActions[func](json.content); 
+};
+
+ws.onopen = function(event) {
+    // Validate the clientCode
+    console.log("UserActions: clientCode needs to be initialized");
+    ws.send(JSON.stringify({
+        "route":  "/client_code",
+        "content":  {
+            "msg_id" :      "123",
+            "client_code" : clientCode
+        }
+    }));
+};
+
+UserActions.ws_client_code.listen(function(content) {
+    console.log("UserActions:ws_client_code ["+content.client_code+"]");
+    clientCode = content.client_code;
+});
+
+
+UserActions.loginWithPassword.listen(function(username, password) {
+    console.log("UserActions:loginWithPassword");
+    ws.send(JSON.stringify({
+        "route":        "/login_with_password",
+        "content": {
+            "client_code" :     clientCode,
+            "username" :        username,
+            "password" :        password
+        }
+    }));
+});
+
+UserActions.register.listen(function(username, password, email) {
+    console.log("UserActions:register");
+    ws.send(JSON.stringify({
+        "route":        "/register",
+        "content": {
+            "client_code":  clientCode,
+            "username":     username,
+            "password":     password,
+            "email":        email
+        }
+    }));
+});
+
 
 module.exports = UserActions;
 
--- src/app/js/stores/UserStore.jsx
+++ src/app/js/stores/UserStore.jsx
@@ -9,11 +9,6 @@ var UserStore = Reflux.createStore({
 
     init: function() {
         this.state = this.getInitialState();
-
-        // TODO this should come from a configuration
-        this.ws = new WebSocket('ws://spacebotwar.com:5000/ws/user/');
-        this.ws.onmessage = this.wsOnMessage;
-        this.ws.onopen = this.wsOpen;
     },
 
     getInitialState: function() {
@@ -28,78 +23,55 @@ var UserStore = Reflux.createStore({
             wsState:        'NOT_CONNECTED'
         };
     },
-    // WebSocket, generic received message router
-    wsOnMessage: function (event) {
-        // TODO Better error checking for valid function names
-        // TODO Check for message error codes
-        console.log("UserStore: message ["+event.data+"]");
-        var json = JSON.parse(event.data);
-        var func = json.route;
-        func = 'ws' + func.replace(/\//g,'_');
-        console.log("Userstore: func ["+func+"]");
-        this[func](json.content);
-    },
-    // WebSocket welcome message
-    ws_: function(data) {
-        console.log("UserStore: ws_");
-    },
-    // WebSocket client_code message
-    ws_client_code: function(data) {
-        console.log("UserStore: ws_client_code");
-        this.clientCode = data.client_code;
-        var userStore = JSON.stringify({
-            clientCode: this.clientCode
-        });
-        console.log("UserStore: set userStore ["+userStore+"]");
-        localStorage.setItem('UserStore', userStore) 
-    },
-
-    // WebSocket connection made
-    wsOpen: function(event) {
-        // TODO Look at doing this with promises.
-        this.state.wsState = 'CONNECTED';
-
-        // Validate the clientCode
-        var clientCode = this.state.clientCode || "bad";
-        console.log("UserStore: clientCode needs to be initialized");
-        this.ws.send(JSON.stringify({
-            "route":  "/client_code",
-            "content":  {
-                "msg_id" :      "123",
-                "client_code" : clientCode
-            }
-        }));
-    },
-
+    
     onLoginWithPassword: function(username, password) {
         console.log("UserStore: loginWithPassword ["+username+"]["+password+"]");
         _.assign(this.state, {
             username:   username,
             password:   password
         });
-        this.ws.send(JSON.stringify({
-            "route":        "/login_with_password",
-            "content": {
-                "client_code" :     this.state.clientCode,
-                "username" :        username,
-                "password" :        password
-            }
-        }));
 
         this.trigger(this.state);
         console.log("UserStore: after trigger");
     },
+    onWs_login_with_password: function(content) {
+        console.log("UserStore: ws_login_with_password");
+        if (content.code === 0) {
+            console.log("UserStore: Success "+content.message);
+            _.assign(this.state, {
+                mode:   'LOGGED_IN'
+            });
+        }
+        else {
+            console.log("UserStore: ERROR "+content.message);
+        }
+    },
+
     onRegister: function(username, password, email) {
         console.log("UserStore: register ["+username+"]["+password+"]["+email+"]");
         _.assign(this.state, {
             username:   username,
             password:   password,
             email:      email,
-            mode:       'LOGGED_IN'
         });
         this.trigger(this.state);
         console.log("UserStore: after trigger");
     },
+    
+    onWs_register: function(content) {
+        console.log("UserStore: ws_register");
+        if (content.code === 0) {
+            console.log("UserStore: Success "+content.message);
+            _.assign(this.state, {
+                mode:   'LOGGED_IN'
+            });
+        }
+        else {
+            console.log("UserStore: ERROR"+content.message);
+        }
+        this.trigger(this.state);
+    },
+
     onLogout: function() {
         console.log("UserStore: logout");
         this.state = this.getInitialState();
